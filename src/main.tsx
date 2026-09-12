9cfeae51e277edd4d4d9a5cbc5d5fd78474971f2
chistocrym-wq <chistocrym@gmail.com>
2026-09-12 07:43:59 +0300

    Load Otto splash styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import './schreiben-theme.css';
+import './otto-splash.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
